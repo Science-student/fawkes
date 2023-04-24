10da0a56e604678264d6875a9c76ffbaa8b6041d
Science-student <122073403+Science-student@users.noreply.github.com>
2023-04-24 20:03:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup_requires = []
 install_requires = [
     'numpy>=1.19.5',
     'tensorflow==2.4.1',
-    'keras==2.4.3',
+    'keras',
     'mtcnn',
     'pillow>=7.0.0',
     'bleach>=2.1.0'
